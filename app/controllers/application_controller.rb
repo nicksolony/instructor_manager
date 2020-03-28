25cdcb57720f161b9f2699b9682033f2a8cc3281
nicksolony <nicksolony@gmail.com>
2020-03-28 15:31:57 -0400

    created login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'password_security'
   end
 
   get "/" do
     erb :welcome
+  end
+
+  get '/login' do
+    erb :login
+  end
+
+  get '/signup' do
+    erb :signup
   end
 
 end
