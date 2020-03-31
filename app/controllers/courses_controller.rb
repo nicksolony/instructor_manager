df6ae41fdfa205e8b51aa76fca484774b6c36865
nicksolony <nicksolony@gmail.com>
2020-03-30 23:48:31 -0400

    added creation of new courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,13 +14,35 @@ class CoursesController < ApplicationController
 
   # GET: /courses/new
   get "/courses/new" do
-    erb :"/courses/new.html"
+    if Helpers.logged_in?(session)
+      @instructor= Helpers.current_user(session)
+      @course_groups=CourseGroup.all.uniq.sort_by(&:name)
+      erb :"/courses/new.html"
+    else
+      redirect to '/courses'
+    end
   end
 
   # POST: /courses
   post "/courses" do
-    redirect "/courses"
-  end
+    @instructor= Helpers.current_user(session)
+    @course=Course.new(params[:course])
+    @course.instructors << @instructor
+    @course.creator_id = @instructor.id
+    if !params[:course_group_name].empty?
+        new_course_group = CourseGroup.create(name: params[:course_group_name], creator_id: @instructor.id)
+        @course.course_group_id = new_course_group.id
+    end
+      if @course.save
+        redirect to "/courses/#{@course.slug}"
+      elsif @course.name==""
+          flash[:message] = "Course Name can't be blank"
+      else
+          flash[:message] = "Course with this name already exist"
+      end
+        redirect to '/courses/new'
+      end
+
 
   # GET: /courses/5
   get "/courses/:id" do
